f0d77d09e57e543864cab1aa0d156b2963a22938
hooosseinbn <hosseinbazrafshan12344321@gmail.com>
2020-08-04 18:56:55 +0430

    new local setting

--- django_udemy2/settings.py
+++ django_udemy2/settings.py
@@ -125,4 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+try:
+    from .local_setting import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
